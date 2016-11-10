29a3a32ce85b2c95ee1745035c70c2799dca3873
Dean Friedland <dfmmalaw@gmail.com>
2016-11-10 18:52:56 +0000

    added ajax call to server whick takes server data, creates an instance of that data, and the pushes new data to observable. Also, added ajax call to save data to server

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ function TaskListViewModel() {
     self.incompleteTasks = ko.computed(function() {
         return ko.utils.arrayFilter(self.tasks(), function(task) { return !task.isDone() });
     });
+    
+    // Load initial state from server, convert it to Task instances, then populate self.tasks.
+    // This code takes the raw data array returned by the server and uses jQuery's $.map helper 
+    // to construct a Task instance from each raw entry. 
+    // The resulting array of Task objects is then pushed into the tasks array, which causes the 
+    // UI to update because it's observable.
+    $.getJSON("/tasks", function(allData) {
+        var mappedTasks = $.map(allData, function(item) { return new Task(item) });
+        self.tasks(mappedTasks);
+    }); 
 
     // Operations
     self.addTask = function() {
